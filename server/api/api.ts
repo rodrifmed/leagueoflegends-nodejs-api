f3e85f40174f99a86f587cd4115b464a6399fe25
rodrifmed <rodrifmed@gmail.com>
2018-11-24 20:22:48 -0800

    response for path /

--- server/api/api.ts
+++ server/api/api.ts
@@ -1,7 +1,9 @@
 
 import { Application } from "express";
 import { apiGetMatches } from "./apiGetMatches";
+import { onSuccess } from "../../examples/node-firebase-rest-api-example/server/api/onSuccess";
 
 export function initRestApi(app: Application) {
+    app.route('/').get((req, res) => onSuccess(res, 'Hello World'));
     app.route('/api/matches/:name').get(apiGetMatches);
 }
--- server/api/onSuccess.ts
+++ server/api/onSuccess.ts
@@ -1,7 +1,5 @@
 import { Response } from 'express';
 
 export function onSuccess(res: Response, data: any) {
-    console.log(data);
     res.status(200).json({ payload: data });
-
 }
